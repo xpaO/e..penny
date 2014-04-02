c5ca7998754ea45c1f647bdc086c7236cd0d09c9
Henrique <coura.henrique@gmail.com>
2014-04-02 14:03:56 -0300

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PennyFound::Application.config.secret_key_base = 'e81a871c7426cba1e76cc1dea7ada15b5b94cbabd9afbed71d0c9b71d0f0406de58b3df50ae40a8d066290ae02788fcea1e4cfa23f72e1c525a94f088d0bef68'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PennyFound::Application.config.session_store :cookie_store, key: '_penny_found_session'
