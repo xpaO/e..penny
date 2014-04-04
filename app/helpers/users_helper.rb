45c0e2d3c2cf84ea94767b52e75a28f4f42a50d4
Henrique <coura.henrique@gmail.com>
2014-04-04 01:42:09 -0300

    Trust mechanic

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+
+  # def site_trusted
+  #   @site_trusted = request.headers["HTTP_EVE_TRUSTED"]
+  # end
+
+  # def site_trusted
+  #   @self = request.headers["HTTP_EVE_TRUSTED"] ? request.headers["HTTP_EVE_TRUSTED"] == "yes" : nil
+  # end
+
+  # def site_trusted?
+  #   site_trusted.nil?
+  # end
 end
